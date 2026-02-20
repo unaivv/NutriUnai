977438411ca3d776e5e93e3eabe673532f1aa89f
Unaivv <vidalvisounai@gmail.com>
2026-02-20 18:52:00 +0100

    configure HTTPS asset prefix for mixed content fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  assetPrefix: 'https://nutriunai.unaividal.com',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'nutriunai.unaividal.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
