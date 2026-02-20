9f9bad929e40f1cb7667a387783d90afdf4ffb56
Unaivv <vidalvisounai@gmail.com>
2026-02-20 19:25:15 +0100

    use absolute path for .env file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
     script: 'npm',
     args: 'start',
     cwd: __dirname,
-    env_file: '/home/pi/NutriUnai/.env',
+    env_file: '.env',
     instances: 1,
     autorestart: true,
     watch: false,
