5dcaf3de4668589f3c4e6b9302c8e94b14c40952
Unaivv <vidalvisounai@gmail.com>
2026-02-20 19:01:55 +0100

    use .env file for environment variables

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,18 +4,7 @@ module.exports = {
     script: 'npm',
     args: 'start',
     cwd: __dirname,
-    env: {
-      NODE_ENV: 'production',
-      PORT: 3001,
-      OPENAI_API_KEY: 'tu-api-key-aqui',
-      JWT_SECRET: 'jqyghGBNub0w5Dr+gHilO6wKdoiYASxNgBNvHUmO7Cs='
-    },
-    env_production: {
-      NODE_ENV: 'production',
-      PORT: 3001,
-      OPENAI_API_KEY: 'tu-api-key-aqui',
-      JWT_SECRET: 'jqyghGBNub0w5Dr+gHilO6wKdoiYASxNgBNvHUmO7Cs='
-    },
+    env_file: '.env',
     instances: 1,
     autorestart: true,
     watch: false,
@@ -24,7 +13,6 @@ module.exports = {
     out_file: './logs/out.log',
     log_file: './logs/combined.log',
     time: true,
-    env_file: '.env',
     merge_logs: true,
     kill_timeout: 5000,
     wait_ready: true,
